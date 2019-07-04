5bd3d697207a83ef9af7c081102f4f09faf5aedf
Tom Stoll <tom@kitefishlabs.com>
2019-07-03 23:41:14 -0400

    Documentation. Drop most console output.

--- color_features.py
+++ color_features.py
@@ -58,7 +58,8 @@ __copyright__ = "Copyright (C) 2019 Thomas Stoll, All Rights Reserved"
 __license__ = "gpl 3.0 or higher"
 __email__ = 'tom@kitefishlabs.com'
 
-import sys, os
+import sys
+import os
 import cv2 as cv
 import numpy as np
 
@@ -66,12 +67,13 @@ import numpy as np
 class ColorFeatures:
     """
     """
+
     def __init__(self, **analysis_params):
         self.initialize(analysis_params)
-    
+
     def initialize(self, analysis_params=None):
         self._check_cf_params(analysis_params)
-    
+
     def _check_cf_params(self, analysis_params=None):
         """
         Simple mechanism to read in default parameters while substituting custom parameters.
@@ -80,7 +82,8 @@ class ColorFeatures:
         dcfp = self.default_cf_params()
         for k in dcfp.keys():
             if k == 'media_file_path':
-                self.analysis_params[k] = os.path.abspath(self.analysis_params.get(k, dcfp[k]))
+                self.analysis_params[k] = os.path.abspath(
+                    self.analysis_params.get(k, dcfp[k]))
             elif (k == 'color_space'):
                 # enforce lab-unless-rgb for color_space
                 if self.analysis_params.get(k, 'lab') != 'rgb':
@@ -94,16 +97,19 @@ class ColorFeatures:
     @staticmethod
     def default_cf_params():
         analysis_params = {
-            'verbose' : False,			# useful for debugging
-            'media_file_path' : os.path.abspath('img/gradient.png'),	# default file
-            'color_space' : 'lab', 		# 'lab' or 'rgb'
-            'grid_divs' : 4,
-            'bins' : 16,				# number of bins for histogram
-            'onset' : 0,				# time onset in seconds (for time-based media, == 0 for images)
-            'threshold' : 0,			# (empirical) threshold for histogram; set to a positive number to remove extremely low values
+            'verbose': False,			# useful for debugging
+            # default file
+            'media_file_path': os.path.abspath('img/gradient.png'),
+            'color_space': 'lab', 		# 'lab' or 'rgb'
+            'grid_divs': 4,
+            'bins': 16,				# number of bins for histogram
+            # time onset in seconds (for time-based media, == 0 for images)
+            'onset': 0,
+            # (empirical) threshold for histogram; set to a positive number to remove extremely low values
+            'threshold': 0,
         }
         return analysis_params
-    
+
     def _analyze_image(self):
         """
         Analyze full image and gridded image according to params set in class creation.
@@ -116,7 +122,8 @@ class ColorFeatures:
         num_histograms = (ap['grid_divs'] ** 2) + 1
 
         img = cv.imread(imgpath)
-        fp = np.memmap(datapath, dtype='float32', mode='w+', shape=(num_histograms, 3, ap['bins']))
+        fp = np.memmap(datapath, dtype='float32', mode='w+',
+                       shape=(num_histograms, 3, ap['bins']))
 
         grid_divs = ap['grid_divs']
         hist_bins = ap['bins']
@@ -126,7 +133,7 @@ class ColorFeatures:
         frame_width = int(img.shape[1])
         grid_width = int(frame_width / grid_divs)
         grid_height = int(frame_height / grid_divs)
-        
+
         # np arrays to hold pixel data after color converson, ititialized with 0s
         img_ = np.zeros((frame_height, frame_width, 3), dtype=np.float32)
         sub_ = np.zeros((grid_height, grid_width, 3), dtype=np.float32)
@@ -140,17 +147,17 @@ class ColorFeatures:
             img_ = cv.cvtColor(img, cv.COLOR_BGR2Lab)
 
         # arrays to hold each color dimension's bins
-        l_star = np.zeros((hist_bins,1), dtype=np.float32)
-        a_star = np.zeros((hist_bins,1), dtype=np.float32)
-        b_star = np.zeros((hist_bins,1), dtype=np.float32)
+        l_star = np.zeros((hist_bins, 1), dtype=np.float32)
+        a_star = np.zeros((hist_bins, 1), dtype=np.float32)
+        b_star = np.zeros((hist_bins, 1), dtype=np.float32)
 
         # analyze entire image - take L2 norm (max = 1.0) each color channel's histogram as we go
-        item = cv.calcHist([img_],[0],None,[hist_bins],[0,hist_range])
-        cv.normalize(np.where(item>thresh,item,0),l_star)
-        item = cv.calcHist([img_],[1],None,[hist_bins],[0,hist_range])
-        cv.normalize(np.where(item>thresh,item,0),a_star)
-        item = cv.calcHist([img_],[2],None,[hist_bins],[0,hist_range])
-        cv.normalize(np.where(item>thresh,item,0),b_star)
+        item = cv.calcHist([img_], [0], None, [hist_bins], [0, hist_range])
+        cv.normalize(np.where(item > thresh, item, 0), l_star)
+        item = cv.calcHist([img_], [1], None, [hist_bins], [0, hist_range])
+        cv.normalize(np.where(item > thresh, item, 0), a_star)
+        item = cv.calcHist([img_], [2], None, [hist_bins], [0, hist_range])
+        cv.normalize(np.where(item > thresh, item, 0), b_star)
 
         # zeroth slot is full image
         fp[0][0] = np.reshape(l_star[:], (hist_bins))
@@ -160,37 +167,42 @@ class ColorFeatures:
         # slice and calculate histograms for sub-images
         for i in range(grid_divs):
             for j in range(grid_divs):
-                
-                sub_lab = np.zeros((grid_height, grid_width, 3), dtype=np.float32)
+
+                sub_lab = np.zeros(
+                    (grid_height, grid_width, 3), dtype=np.float32)
 
                 # arrays to hold each color dimension's bins
-                l_star = np.zeros((hist_bins,1), dtype=np.float32)
-                a_star = np.zeros((hist_bins,1), dtype=np.float32)
-                b_star = np.zeros((hist_bins,1), dtype=np.float32)
-                
-                sub_ = img_[(i*grid_height):((i+1)*grid_height),(j*grid_width):((j+1)*grid_width)]
-                                
-                item = cv.calcHist([sub_],[0],None,[hist_bins],[0,hist_range])
-                cv.normalize(np.where(item>thresh,item,0),l_star)
-                item = cv.calcHist([sub_],[1],None,[hist_bins],[0,hist_range])
-                cv.normalize(np.where(item>thresh,item,0),a_star)
-                item = cv.calcHist([sub_],[2],None,[hist_bins],[0,hist_range])
-                cv.normalize(np.where(item>thresh,item,0),b_star)
-                        
-                fp[ ((grid_divs*i)+j) + 1 ][0] = np.reshape(l_star[:], (hist_bins))
-                fp[ ((grid_divs*i)+j) + 1 ][1] = np.reshape(a_star[:], (hist_bins))
-                fp[ ((grid_divs*i)+j) + 1 ][2] = np.reshape(b_star[:], (hist_bins))
-                
+                l_star = np.zeros((hist_bins, 1), dtype=np.float32)
+                a_star = np.zeros((hist_bins, 1), dtype=np.float32)
+                b_star = np.zeros((hist_bins, 1), dtype=np.float32)
+
+                sub_ = img_[(i*grid_height):((i+1)*grid_height),
+                            (j*grid_width):((j+1)*grid_width)]
+
+                item = cv.calcHist([sub_], [0], None, [
+                                   hist_bins], [0, hist_range])
+                cv.normalize(np.where(item > thresh, item, 0), l_star)
+                item = cv.calcHist([sub_], [1], None, [
+                                   hist_bins], [0, hist_range])
+                cv.normalize(np.where(item > thresh, item, 0), a_star)
+                item = cv.calcHist([sub_], [2], None, [
+                                   hist_bins], [0, hist_range])
+                cv.normalize(np.where(item > thresh, item, 0), b_star)
+
+                fp[((grid_divs*i)+j) + 1][0] = np.reshape(l_star[:], (hist_bins))
+                fp[((grid_divs*i)+j) + 1][1] = np.reshape(a_star[:], (hist_bins))
+                fp[((grid_divs*i)+j) + 1][2] = np.reshape(b_star[:], (hist_bins))
+
         fp.flush()
         return img.shape
-
 
     def get_analysis_result(self):
         """
         print an image's entire data array
         """
         ap = self.analysis_params
-        datapath = os.path.abspath(self.analysis_params['media_file_path']+'.'+ap['color_space'])
-        print('Access ' + datapath)
-        fp = np.memmap(datapath, dtype='float32', mode='r+', shape=(((ap['grid_divs'] ** 2) + 1), 3, ap['bins']))
+        datapath = os.path.abspath(
+            self.analysis_params['media_file_path']+'.'+ap['color_space'])
+        fp = np.memmap(datapath, dtype='float32', mode='r+',
+                       shape=(((ap['grid_divs'] ** 2) + 1), 3, ap['bins']))
         return fp
--- color_features_pyramid.py
+++ color_features_pyramid.py
@@ -7,11 +7,15 @@ Part of ImageDB2 - Tools for analysis, indexing, search, and creative use of ima
 
 Overview
 ========
-todo
+Pyramid down-sampling + color-feature analysis.
+Saves downsampled data to NAME.COLORSPACE.PYR{DOWNSAMPLING} data files mem-mapped to np arrays.
+Also retrieves and displays down-sampled image data.
 
 """
 
-import sys, os, math
+import sys
+import os
+import math
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
@@ -20,12 +24,13 @@ import matplotlib.pyplot as plt
 class ColorFeaturesPyramid:
     """
     """
+
     def __init__(self, **analysis_params):
         self.initialize(analysis_params)
-    
+
     def initialize(self, analysis_params=None):
         self._check_cf_params(analysis_params)
-    
+
     def _check_cf_params(self, analysis_params=None):
         """
         Simple mechanism to read in default parameters while substituting custom parameters.
@@ -34,7 +39,8 @@ class ColorFeaturesPyramid:
         dcfp = self.default_cf_params()
         for k in dcfp.keys():
             if k == 'media_file_path':
-                self.analysis_params[k] = os.path.abspath(self.analysis_params.get(k, dcfp[k]))
+                self.analysis_params[k] = os.path.abspath(
+                    self.analysis_params.get(k, dcfp[k]))
             elif (k == 'color_space'):
                 # enforce lab-unless-rgb for color_space
                 if self.analysis_params.get(k, 'lab') != 'rgb':
@@ -48,20 +54,24 @@ class ColorFeaturesPyramid:
     @staticmethod
     def default_cf_params():
         analysis_params = {
-            'verbose' : False,			# useful for debugging
-            'media_file_path' : os.path.abspath('img/gradient.png'),	# default file
-            'color_space' : 'lab', 		# 'lab' or 'rgb'
-            'down_sampling' : 2,        # multiple of 2
-            'onset' : 0,				# time onset in seconds (for time-based media, == 0 for images)
+            'verbose': False,			# useful for debugging
+            # default file
+            'media_file_path': os.path.abspath('img/gradient.png'),
+            'color_space': 'lab', 		# 'lab' or 'rgb'
+            'down_sampling': 2,        # multiple of 2
+            # time onset in seconds (for time-based media, == 0 for images)
+            'onset': 0,
         }
         return analysis_params
-    
+
     def _down_sample_and_analyze(self):
         """
+        Reads and image, performs a series of down-samplings, writes each to disk
         """
         ap = self.analysis_params
         imgpath = ap['media_file_path']
-        print('Down-sample + analyze: ' + imgpath + ' (' + str(ap['down_sampling']) + ')')
+        print('Down-sample + analyze: ' + imgpath +
+              ' (' + str(ap['down_sampling']) + ')')
         datapath = ap['media_file_path'] + '.' + ap['color_space'] + '.pyr'
 
         img = cv.imread(imgpath)
@@ -71,46 +81,45 @@ class ColorFeaturesPyramid:
 
         # convert color space once
         if ap['color_space'] == 'rgb':
-            print('2 rgb')
             img_ = cv.cvtColor(img, cv.COLOR_BGR2RGB)
         else:
-            print('2 lab')
             img_ = cv.cvtColor(img, cv.COLOR_BGR2Lab)
 
         # create a list of np arrays to hold our gradually shrinking arrays
-        imgs_ds = [img_] + [np.zeros((int(imgh / math.pow(2, (level+1))), int(imgw / math.pow(2, (level+1))), 3), dtype=np.uint8) for level in range(ds_levels)]
+        imgs_ds = [img_] + [np.zeros((int(imgh / math.pow(2, (level+1))), int(
+            imgw / math.pow(2, (level+1))), 3), dtype=np.uint8) for level in range(ds_levels)]
 
         for level in range(ds_levels):
-            print('level: ', str(level))
-            print(imgs_ds[level].shape)
-            print('dst path: ', (datapath + str(level+1)))
-            print(imgs_ds[level+1].shape)
             imgs_ds[level+1] = cv.pyrDown(imgs_ds[level], imgs_ds[level+1])
-        
+
         for level in range(ds_levels):
-            fp = np.memmap((datapath + str(level+1)), dtype='uint8', mode='w+', shape=(imgs_ds[level+1].shape[0], imgs_ds[level+1].shape[1], 3))
+            fp = np.memmap((datapath + str(level+1)), dtype='uint8', mode='w+',
+                           shape=(imgs_ds[level+1].shape[0], imgs_ds[level+1].shape[1], 3))
             fp[:] = np.copy(imgs_ds[level+1])
             fp.flush()
-        
-        return (imgh, imgw)
-    
+
     def load_and_display(self, ds=8):
+        """
+        """
         ap = self.analysis_params
         imgpath = ap['media_file_path']
         lvl = int(math.log2(ds))
-        datapath = ap['media_file_path'] + '.' + ap['color_space'] + '.pyr' + str(lvl)
-        print('Display down-sampled: ' + datapath)
+        datapath = ap['media_file_path'] + '.' + \
+            ap['color_space'] + '.pyr' + str(lvl)
         img = cv.imread(imgpath)
-        fp = np.memmap(datapath, dtype='uint8', mode='r+', shape=(int(img.shape[0] / math.pow(2, lvl)), int(img.shape[1] / math.pow(2, lvl)), 3))
+        fp = np.memmap(datapath, dtype='uint8', mode='r+', shape=(
+            int(img.shape[0] / math.pow(2, lvl)), int(img.shape[1] / math.pow(2, lvl)), 3))
         plt.imshow(fp)
         plt.show()
-        return (fp, img.shape, fp.shape)
-    
+
     def get_downsampled_data(self, lvl=8):
+        """
+        """
         ap = self.analysis_params
         imgpath = ap['media_file_path']
-        datapath = ap['media_file_path'] + '.' + ap['color_space'] + '.pyr' + str(lvl)
-        print('Read down-sampled: ' + datapath)
+        datapath = ap['media_file_path'] + '.' + \
+            ap['color_space'] + '.pyr' + str(lvl)
         img = cv.imread(imgpath)
-        fp = np.memmap(datapath, dtype='uint8', mode='r+', shape=(int(img.shape[0] / math.pow(2, lvl)), int(img.shape[1] / math.pow(2, lvl)), 3))
-        return (fp, img.shape, fp.shape)
+        fp = np.memmap(datapath, dtype='uint8', mode='r+', shape=(
+            int(img.shape[0] / math.pow(2, lvl)), int(img.shape[1] / math.pow(2, lvl)), 3))
+        return fp
